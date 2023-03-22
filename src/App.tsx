423c15ad84ede8c5e3006e831e8c45139ea2b405
appaandbuki <104958634+rachelCilla@users.noreply.github.com>
2023-03-22 16:07:29 -0600

    answer component renders

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,15 @@ import React, {useState, useEffect} from 'react';
 import Title from './components/Title';
 import { QuizData, Content } from '../interfaces';
 import QuestionsBlock from './components/QuestionsBlock';
+import AnswerBlock from './components/AnswerBlock';
 
 function App() {
 
   const [quiz, setQuiz] = useState<QuizData | null>()
   const [chosenAnswerItems, setChosenAnswerItems] = useState<string[]>([])
  const [unansweredQuestionIds, setUnansweredQuestionIds] = useState<number[]| undefined>([])
+const [showAnswer, setShowAnswer] = useState<boolean>(false)
+
 
   const fetchData = async () => {
     try { 
@@ -34,11 +37,18 @@ console.log(unansweredQuestionIds)
 // scroll to highest unanswered question (by id)
 useEffect(() => {
   if (unansweredQuestionIds) {
+    if (unansweredQuestionIds.length <= 0 && chosenAnswerItems.length>= 1) {
+      setShowAnswer(true)
+      const anwserBlock= document.getElementById('answer-block')
+      anwserBlock?.scrollIntoView({behavior: 'smooth'})
+    }
+
+
   const highestId= Math.min(...unansweredQuestionIds)
   const highestElement = document.getElementById(String(highestId))
   highestElement?.scrollIntoView({behavior: 'smooth'})
   }
-},[unansweredQuestionIds])
+},[unansweredQuestionIds, chosenAnswerItems, showAnswer])
 
 
 //  Renders the Title, Subtitle, and a series of questions represented by 'QuestionsBlock' components
@@ -55,8 +65,9 @@ useEffect(() => {
         setUnansweredQuestionIds={setUnansweredQuestionIds}
         unansweredQuestionIds={unansweredQuestionIds}
         chosenAnswerItems={chosenAnswerItems}
-      
-    />))}
+    />
+    ))}
+    {showAnswer && <AnswerBlock answerOptions={quiz?.answers} chosenAnswers={chosenAnswerItems}/>}
     </div>
   );
 }
--- src/components/AnswerBlock.tsx
+++ src/components/AnswerBlock.tsx
@@ -1,7 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Answer } from "../../interfaces";
 
-function AnswerBlock() {
-  return <div></div>;
+function AnswerBlock({
+  answerOptions,
+  chosenAnswers
+}:{
+  answerOptions: Answer[] | undefined,
+  chosenAnswers: string[]
+}
+) {
+
+  const [result, setResult] = useState<Answer | null>()
+
+useEffect(() => {
+  answerOptions?.forEach((answer:Answer) => {
+    if (
+      chosenAnswers.includes(answer.combination[0] )&&
+      chosenAnswers.includes(answer.combination[1]) &&
+        chosenAnswers.includes(answer.combination[2])
+   ) {
+
+      setResult(answer)
+  }
+  })
+},[chosenAnswers])
+
+console.log(result)
+
+
+
+  return ( 
+  <div id="answer-block" className="answer-block"> 
+    <h2>{result?.text}</h2>
+    <img src={result?.image} alt={result?.text}/>
+  </div>
+  );
 }
 
 export default AnswerBlock;
